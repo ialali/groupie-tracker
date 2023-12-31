7e1aeced47d475d292338ebd12fd15219a97616e
Ibrahim Al-Ali <ibrahim.alali94@yahoo.ro>
2023-12-31 10:19:40 +0000

    a

--- handlers/handlehomepage.go
+++ handlers/handlehomepage.go
@@ -15,14 +15,10 @@ func Homepage(w http.ResponseWriter, r *http.Request) {
 	// Check if the request is a form submission
 	if r.Method == http.MethodGet {
 		// Get the search query from the form submission
-		query := r.URL.Query().Get("query")
-
-		// Use the PerformSearch function to perform the search
-		results := PerformSearch(query)
 
 		// Create a map with the search results to pass to the template
 		data := map[string]interface{}{
-			"Artists": results,
+			"Artists": global.Artists,
 		}
 
 		// Serve the HTML page with the search results
